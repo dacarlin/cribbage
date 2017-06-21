945fc5d6e98f1b1b9f07e2ccc7e12d5167804356
Alex Carlin <carlin@ucdavis.edu>
2017-06-21 01:59:34 -0700

    Updated main with more intuitive options

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 from game import game
 from argparse import ArgumentParser
-from players import HumanPlayer, NondeterministicAIPlayer, EnumerativeAIPlayer
+from players import (HumanPlayer, NondeterministicAIPlayer, EnumerativeAIPlayer, 
+                     AIPlayer) 
 
 player_choices = {
     'human': HumanPlayer(),
-    'ai': EnumerativeAIPlayer(),
+    'enumerative': EnumerativeAIPlayer(),
+    'ai': AIPlayer(),
     'random': NondeterministicAIPlayer(),
 }
 
 parser = ArgumentParser()
 parser.add_argument('player1', help='Player 1', choices=player_choices)
 parser.add_argument('player2', help='Player 2', choices=player_choices)
-parser.add_argument('--debug', help="Enable debugging mode", type=bool, default=False)
-# parser.add_argument('--gui', help="Enable GUI", type=bool, default=True)
-parser.add_argument('--gui', type=lambda s: s.lower() in ['true', 't', 'yes', '1'], help="Enable GUI")
+parser.add_argument('--debug', help="Enable debug mode", action="store_true")
+parser.add_argument('--no-gui', help="Disable GUI", action="store_false")
 args = parser.parse_args()
 
-def main():
+print(args)
 
+def main():
     players = (player_choices[args.player1], player_choices[args.player2])
-    score = game(players, debug=args.debug, gui=args.gui)
+    score = game(players, debug=args.debug, gui=args.no_gui)
     print('The final score is', score)
 
 if __name__ == '__main__':
