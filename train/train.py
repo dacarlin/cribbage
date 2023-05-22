25eaec74e1fd22cd535bca9eead40672d95ca936
Alex Carlin <alex@mac.local>
2023-05-21 23:59:21 -0700

    Add skeleton of training script

---
+++ train/train.py
@@ -0,0 +1,16 @@
+from cribbage.game import Game
+from cribbage.players import NondeterministicAIPlayer, WinGame
+
+
+def main():
+    player_1 = NondeterministicAIPlayer(name="Player 1")
+    player_2 = NondeterministicAIPlayer(name="Player 2")
+    game = Game(player_1, player_2)
+    try:
+        game.run()
+    except WinGame as win_game:
+        print(win_game)
+
+
+if __name__ == "__main__":
+    main() 
